88e7926e845b76104de15e802cf761d345466c80
moz6go <moz6go@gmail.com>
2019-06-08 08:16:34 +0300

    some changes in calendarwidget

--- mycalendarwidget.cpp
+++ mycalendarwidget.cpp
@@ -1,5 +1,5 @@
 #include "mycalendarwidget.h"
-
+#include <QtDebug>
 MyCalendarWidget::MyCalendarWidget(QWidget *parent) : QCalendarWidget (parent) {
     curr_date = QDate::currentDate();
     blue_pen.setColor (Qt::blue);
@@ -16,18 +16,14 @@ void MyCalendarWidget::SetDataList(const QList<QDate> &list) {
 void MyCalendarWidget::paintCell (QPainter *painter, const QRect &rect, const QDate &date) const
 {
     QCalendarWidget::paintCell(painter, rect, date);
-
     if (date == curr_date) {
         painter->setPen(blue_pen);
         painter->setBrush(brush);
-        painter->drawRect(rect.adjusted(0, 0, -1, -1));
+        painter->drawRect(rect.adjusted (0, 0, -1, -1));
     }
-
-    for (auto& d : date_list) {
-        if (d == date){
-            painter->setPen(red_pen);
-            painter->setBrush(brush);
-            painter->drawRect(rect.adjusted(0, 0, -1, -1));
-        }
+    if(date_list.contains (date)){
+        painter->setPen(red_pen);
+        painter->setBrush(brush);
+        painter->drawRect(rect.adjusted (0, 0, -1, -1));
     }
 }
